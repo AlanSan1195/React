962c27effb523af29fe8d61c0d3b9de3b198c556
Alan San <alansandovalt1@gmail.com>
2024-10-14 23:35:38 -0600

    refactorizacion de localStorage y uso de useEffect

--- proyects/02-game-tic-toc/src/App.jsx
+++ proyects/02-game-tic-toc/src/App.jsx
@@ -1,35 +1,38 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Cuadrado } from "./components/Cuadrado";
 import { Modal } from "./Modal";
-import { TURNS} from "./components/Constantes";
-import {checkWinner} from "./components/LogicWinner"
+import { TURNS } from "./components/Constantes";
+import { checkWinner } from "./components/LogicWinner";
 import confetti from "canvas-confetti";
-
-
-
-  
-
+import { saveGameStorage, resetGameStorage } from "./components/storage";
 function App() {
+  // estados con el que inicia un app, aprendimos a inicializar un estado con todo y localStorgafe
+  const [board, setBoard] = useState(() => {
+    const continueGame = window.localStorage.getItem("board");
+    return continueGame ? JSON.parse(continueGame) : Array(9).fill(null);
+  });
 
-  // estados con el que inicia un app
-  const [turn, setTurn] = useState(TURNS.X);
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(() => {
+    const continueTurn = window.localStorage.getItem("turn");
+    return continueTurn ?? TURNS.X;
+  });
   const [winner, setWinner] = useState(null);
 
-// RESETEAR
-// en React esto es asi podemos restablecer filtros busquedas y 
-//  cualquier cosa  solo con regresar los estados a sus valores 
-// iniciales 
-  function resetGame () {
+  // RESETEAR
+  // en React esto es asi podemos restablecer filtros busquedas y
+  //  cualquier cosa  solo con regresar los estados a sus valores
+  // iniciales
+  function resetGame() {
+    setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
-    setBoard(Array(9).fill(null))
-    setWinner(null)
+    setWinner(null);
+    resetGameStorage();
   }
 
-  const checkEndGame = (newBoard)=>{
-    return newBoard.every((cuadrado)=>cuadrado !== null)
-  }
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((cuadrado) => cuadrado !== null);
+  };
 
   //actualiza el turno y el tablero
   const update = (index) => {
@@ -41,21 +44,29 @@ function App() {
     //turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
-    //ganador
+    
+
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
-      confetti()
+      confetti();
       setWinner(newWinner);
       console.log(winner);
-    } else if (checkEndGame(newBoard) ){
-      setWinner(false)
-
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
     }
   };
+  useEffect(() => {
+    // aqui guarda la partida
+    const newTurn = turn
+    const newBoard = board
+    saveGameStorage({ turn: newTurn, board: newBoard });
+    console.log("probando useEfect");
+  },[turn, board]);
 
   return (
     <main className="board">
       <h1>Gato Game</h1>
+      <button onClick={resetGame}>Reset Game</button>
       <section className="game">
         {board.map((_, index) => {
           return (
@@ -71,10 +82,6 @@ function App() {
       </section>
 
       <Modal winner={winner} resetGame={resetGame} />
-
-
-
-      
     </main>
   );
 }
