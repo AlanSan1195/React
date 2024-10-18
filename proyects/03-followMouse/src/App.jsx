a61e5e0afa53b8015f25a58c88ffdd63e3dc01f7
Alan San <alansandovalt1@gmail.com>
2024-10-17 18:23:01 -0600

    create followMouse

---
+++ proyects/03-followMouse/src/App.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+const MouseFollow = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [posision, setPosision] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    console.log("activaste efeccto ", { enabled });
+
+    const eventoMouse = (event) => {
+      const { clientX, clientY } = event;
+      setPosision({ x: clientX, y: clientY });
+    };
+    if (enabled) {
+      window.addEventListener("pointermove", eventoMouse);
+    }
+    return () => {
+      window.removeEventListener("pointermove", eventoMouse);
+    };
+  }, [enabled]);
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#09f",
+          borderRadius: "50%",
+          pointerEvents: "none",
+          opacity: 0.7,
+          height: 40,
+          width: 40,
+          left: -20,
+          top: -20,
+          transform: `translate(${posision.x}px, ${posision.y}px)`,
+        }}
+      />
+      <button
+        onClick={() => {
+          setEnabled(!enabled);
+        }}
+      >
+        {enabled ? "Desactivar" : "Activar"} evento
+      </button>
+    </>
+  );
+};
+
+function App() {
+  const [montado, setDesmontar] = useState(false);
+
+  return (
+    <main className="componenetes">
+      {montado && <MouseFollow />}
+      <button
+        onClick={() => {
+          setDesmontar(!montado);
+        }}
+      >
+        {montado ? "Desmontar" : "Montar"} componente
+      </button>
+    </main>
+  );
+}
+
+export default App;
